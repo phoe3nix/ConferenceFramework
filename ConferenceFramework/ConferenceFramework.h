840fd4bc39a175fafed12ad170a6a75348e49011
Vladislav Usachev <Usachov.V.An@sberbank.ru>
2019-02-24 17:26:50 +0300

    Initial Commit

---
+++ ConferenceFramework/ConferenceFramework.h
@@ -0,0 +1,19 @@
+//
+//  ConferenceFramework.h
+//  ConferenceFramework
+//
+//  Created by Владислав Усачёв on 24/02/2019.
+//  Copyright © 2019 SberTech. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for ConferenceFramework.
+FOUNDATION_EXPORT double ConferenceFrameworkVersionNumber;
+
+//! Project version string for ConferenceFramework.
+FOUNDATION_EXPORT const unsigned char ConferenceFrameworkVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <ConferenceFramework/PublicHeader.h>
+
+
